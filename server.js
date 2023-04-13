a0431d6368742dfbde8c2c39cd046103716bb114
JJ <jjcasey4@hotmail.com>
2023-04-13 11:46:44 +0100

    removed await

--- server.js
+++ server.js
@@ -36,26 +36,11 @@ app.post('/execute', (req, res) => {
   }
 });
 
-      // Wait for the container to finish executing the code
-      await container.wait();
-  
-      // Clean up the container
-      await container.remove();
-  
-      // Send the output back to the client
-      res.json({ output });
-    } catch (error) {
-      console.error('Error details:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-  
-  app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
