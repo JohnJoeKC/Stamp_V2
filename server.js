1272272d4e843d73f4eb2329775146dd03219773
JJ <jjcasey4@hotmail.com>
2023-04-13 11:16:28 +0100

    Added route for serving index.html

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ app.post('/execute', async (req, res) => {
   
   });
 
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
   app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
