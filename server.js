92cf82c282eddc14bb90db51041f858f5fd6ed1c
JJ <jjcasey4@hotmail.com>
2023-04-13 11:12:08 +0100

    adjusted 404 down

--- server.js
+++ server.js
@@ -58,13 +58,13 @@ app.post('/execute', async (req, res) => {
   
   });
 
-  app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-  });
-  
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-  });
-  
   app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
